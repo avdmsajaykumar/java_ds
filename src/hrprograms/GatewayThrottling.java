69f2519f9bda63ff73a703b8f3ab727e58f7a3ee
avdmsajaykumar <avdmsajaykumar@users.noreply.github.com>
2021-04-01 12:10:55 +0530

    Added below Hacker Rank programs
    GatewayThrottling
    StringsToAscendingFormat

---
+++ src/hrprograms/GatewayThrottling.java
@@ -0,0 +1,72 @@
+package hrprograms;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GatewayThrottling {
+
+    List<Integer> input = Arrays.asList(1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7);
+    GatewayThrottling(){
+        System.out.println(Result.droppedRequests(input));
+    }
+
+    public static void main(String[] args) {
+        new GatewayThrottling();
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'droppedRequests' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY requestTime as parameter.
+     */
+
+    public static int droppedRequests(List<Integer> requestTime) {
+        // Write your code here
+        System.out.println(requestTime.toString());
+        int output = 0;
+        Map<Integer, Integer> droppedAlready = new HashMap<>();
+
+        for(int i = 0; i<requestTime.size(); i++){
+            if(i>2 && requestTime.get(i).equals(requestTime.get(i - 3))){
+                if(!droppedAlready.containsKey(requestTime.get(i))
+                        || droppedAlready.get(requestTime.get(i))!=i){
+                    droppedAlready.put(requestTime.get(i), i);
+                    output++;
+                    System.out.println(requestTime.get(i));
+                }
+
+            }
+
+            if(i>19 && (requestTime.get(i) - requestTime.get(i-20))<10){
+                if(!droppedAlready.containsKey(requestTime.get(i))
+                        || droppedAlready.get(requestTime.get(i))!=i){
+                    droppedAlready.put(requestTime.get(i), i);
+                    output++;
+                    System.out.println(requestTime.get(i));
+                }
+
+            }
+
+            if(i>59 && (requestTime.get(i) - requestTime.get(i-60))<60){
+                if(!droppedAlready.containsKey(requestTime.get(i))
+                        || droppedAlready.get(requestTime.get(i))!=i){
+                    droppedAlready.put(requestTime.get(i), i);
+                    output++;
+                    System.out.println(requestTime.get(i));
+                }
+
+            }
+        }
+
+
+        return output;
+    }
+
+}
---
+++ src/hrprograms/StringsToAscendingFormat.java
@@ -0,0 +1,61 @@
+package hrprograms;
+
+public class StringsToAscendingFormat {
+
+    String input = "Hello I'm Atmakuru Venkata Durga Maruthi Shanmuka Ajay Kumar.";
+    public StringsToAscendingFormat() {
+        System.out.println(Arrange.arrange(input));
+    }
+
+    public static void main(String[] args) {
+        new StringsToAscendingFormat();
+    }
+}
+
+
+class Arrange {
+
+    /*
+     * Complete the 'arrange' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING sentence as parameter.
+     */
+
+    public static String arrange(String sentence) {
+        // Write your code here
+        System.out.println(sentence);
+        String output = "";
+
+        if(sentence != null){
+            int maxLength = 0;
+            String[] words = sentence.split(" ");
+            for (String s : words) {
+                if (s.length() > maxLength) maxLength = s.length();
+            }
+
+            int templength = 1;
+            while(templength<=maxLength){
+                for (String s : words) {
+                    String word = s;
+                    word = word.toLowerCase();
+                    word = word.replaceAll("\\.", "");
+
+                    if (word.length() == templength) {
+                        if (output.equals("")) {
+                            output = word.substring(0, 1).toUpperCase() + word.substring(1);
+                        } else {
+                            output = output.concat(" " + word);
+                        }
+                    }
+                }
+                templength++;
+            }
+
+        }
+
+        return output + ".";
+
+    }
+
+}
